41559e281b3ace624a9b9bdb76d100b53e9f65b4
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-17 11:01:40 +0200

    Create nodes
    add construct, gett

--- src/com/nodes.java
+++ src/com/nodes.java
@@ -1,4 +1,30 @@
 package com;
 
 public class nodes {
+    private String name;
+    private nodes node1;
+    private nodes node2;
+
+    public nodes(String name) {
+        this.name = name;
+    }
+
+    public nodes(nodes node1, nodes node2) {
+        this.node1 = node1;
+        this.node2 = node2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public nodes getNode1() {
+        return node1;
+    }
+
+    public nodes getNode2() {
+        return node2;
+    }
+
+
 }
