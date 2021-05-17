39e87166e2706ec9f846604699b6a6f9c4e78806
colinvlienden <colinvlienden@users.noreply.github.com>
2021-05-17 12:08:12 +0200

    Create class FSM
    add construct, implement run(), add toString() output

--- src/com/fsm.java
+++ src/com/fsm.java
@@ -1,4 +1,35 @@
 package com;
+import java.util.List;
+import java.util.ArrayList;
 
 public class fsm {
+    private final nodes run;
+    private final String input;
+
+    public fsm(nodes run, String input) {
+        this.run = run;
+        this.input = input;
+    }
+
+    List runFsm() {
+        List<String> nodes = new ArrayList();
+        nodes State = run;
+        nodes.add(State.getName());
+
+        for (char character: input.toCharArray()) {
+            nodes newNode = State.nextState("" + character + "");
+            nodes.add(newNode.getName());
+            State = newNode;
+
+        }
+        return nodes;
+    }
+
+    @Override
+    public String toString() {
+        return  "RUN FSM {" +
+                "\ninput: " + input +
+                "\noutput: " + runFsm() +
+                "\n}";
+    }
 }
